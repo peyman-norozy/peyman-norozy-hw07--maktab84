4bad4604b183c63cc513d9d5b6ad42d4068d17a1
Peyman Norozi <peyman.norozy@gmail.com>
2022-10-25 01:59:30 -0700

    answer the question two

--- script.js
+++ script.js
@@ -1,29 +1,16 @@
-let salaries = {
-  peyman: 333,
-  John: 100,
-  Pete: 3000,
-  Mary: 2500,
-};
+// const array1 = [5, 6, 2];
+// const array2 = [3, 7, 1];
+// const array3 = [2, 4, 8];
 
-function topSalary(sls) {
-  let maxCash = Object.entries(sls).sort((a, b) => {
-    return a[1] - b[1];
-  });
-  console.log(maxCash);
-  const len = maxCash.length;
-  return maxCash[len - 1][0];
+const array = [
+  [5, 6, 2],
+  [3, 7, 1],
+  [2, 4, 8],
+];
+
+function sort(arr) {
+  let arrayTotal = [];
+  arr.map((item) => arrayTotal.push(...item));
+  return arrayTotal.sort((a, b) => b - a).join();
 }
-console.log(topSalary(salaries));
-// let obj = {};
-
-// function calling() {
-//   obj[prompt("enter your key:")] = +prompt("enter your number:");
-//   const stop = prompt("enter your situation:");
-//   if (stop === "no") {
-//     console.log(obj);
-//   } else {
-//     calling();
-//   }
-// }
-
-// calling();
+console.log(sort(array));
