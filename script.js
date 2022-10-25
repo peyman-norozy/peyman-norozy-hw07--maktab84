01300c05b9a309c9d1b77c3d92888e3cd8cde650
Peyman Norozi <peyman.norozy@gmail.com>
2022-10-25 05:23:48 -0700

    answer question four

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
-let values = [
-  "Hare",
-  "Krishna",
-  "Hare",
-  "Krishna",
-  "Krishna",
-  "Krishna",
-  "Hare",
-  "Hare",
-  ":-O",
-  1,
-  1,
-  2,
-  true,
-  true,
-  false,
-];
-
-function unique(arr) {
-  let array = [];
-  const ordersSet = new Set(arr);
-  ordersSet.forEach((item) => array.push(item));
-  return array;
+let arr = ["nap", "teachers", "PAN", "cheaters", "ear", "era", "hectares"];
+function groupAnagrams(strs) {
+  let result = {};
+  let finall = [];
+  for (let word of strs) {
+    let cleansed = word.toLowerCase().split("").sort().join("");
+    console.log(cleansed);
+    if (result[cleansed]) {
+      result[cleansed].push(word);
+    } else {
+      result[cleansed] = [word];
+    }
+  }
+  console.log(result);
+  let anagrams = Object.values(result);
+  console.log(anagrams);
+  anagrams.forEach((item) => finall.push(item[0]));
+  return finall.join();
 }
+alert(groupAnagrams(arr));
 
-console.log(unique(values));
+// let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+// const groupAnagrams = (strs) => {
+//   const anagrams = {};
+
+//   for (let i = 0; i < strs.length; i++) {
+//     const singleString = strs[i];
+//     const sortedString = [...singleString].sort().join("");
+//     if (!anagrams[sortedString]) anagrams[sortedString] = [];
+//     anagrams[sortedString].push(singleString);
+//   }
+//   return Object.values(anagrams);
+// };
+
+// console.log(groupAnagrams(arr));
